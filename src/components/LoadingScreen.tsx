a3d936d51b153ae05b9cd229570e59516fcfe660
Dániel Erdős <13763460+daniel-erdos-dev@users.noreply.github.com>
2025-02-06 17:31:14 +0100

    adding env secrets to github secrets

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -28,7 +28,6 @@ export const LoadingScreen: React.FC<LoadingScreenProps> = ({ onComplete }) => {
         {text}
         <span className="animate-blink ml-1"> | </span>
       </div>
-
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
